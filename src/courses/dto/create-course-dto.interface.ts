700d4398a714c083c30c10046d46edcf5183aa1e
Arlin-Mafra <arlinsantana@gmail.com>
2021-07-04 18:49:56 -0300

    use validation pipe

--- src/courses/dto/create-course-dto.interface.ts
+++ src/courses/dto/create-course-dto.interface.ts
@@ -1,5 +1,12 @@
-export interface CreateCourseDto {
+import { IsString } from 'class-validator';
+
+export class CreateCourseDto {
+  @IsString()
   readonly name: string;
+
+  @IsString()
   readonly description: string;
+
+  @IsString({ each: true })
   readonly tags: string[];
 }
--- src/courses/dto/update-course-dto.interface.ts
+++ src/courses/dto/update-course-dto.interface.ts
@@ -1,6 +1,14 @@
-export interface UpdateCourseDto {
+import { IsString } from 'class-validator';
+
+export class UpdateCourseDto {
   id?: number;
+
+  @IsString()
   readonly name?: string;
+
+  @IsString()
   readonly description?: string;
+
+  @IsString({ each: true })
   readonly tags?: string[];
 }
