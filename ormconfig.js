c45fac3fbd95d732f5dcc0b73178d2d7a49b7589
Arlin-Mafra <arlinsantana@gmail.com>
2021-08-27 10:37:52 -0300

    ssl double check

--- ormconfig.js
+++ ormconfig.js
@@ -2,6 +2,10 @@ console.log('process.env.DATABASEE_URL :>> ', process.env.DATABASE_URL);
 module.exports = {
   type: 'postgres',
   url: process.env.DATABASE_URL,
+  sl: true, // double check
+  extra: {
+    ssl: true,
+  },
   entities: ['dist/**/*.entity.js'],
   migrations: ['dist/migrations/*.js'],
   cli: {
